21a49ef09d3c97ff6fdac075ea51a3a9a757b4fb
linhan <scorpio360@163.com>
2019-07-28 19:34:58 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div class="test">
+      wocao
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.test {
+  border: 10px;
+  font-size: 1rem;
+  width: 10rem;
+  height: 1rem;
+  border: 1px solid #000;
+}
+</style>
